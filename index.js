7dc53ff8336da7f02f2ab6e8c79018a2bee22bfe
vincentbollaert <vincentbollaert@gmail.com>
2020-07-21 10:09:41 +0700

    added more routes, minor refactor

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-const express = require('express')
-// const http = require('http')
-const fs = require('fs')
-const bodyParser = require('body-parser')
+import express from 'express'
+import fs from 'fs'
+import bodyParser from 'body-parser'
+import path from 'path'
+import { v4 as uuid } from 'uuid'
+import { success, failure } from './src/utils.js'
 
-// const url = require('url')
+const __dirname = path.resolve()
+const todos = JSON.parse(fs.readFileSync(`${__dirname}/src/data/todos.json`))
 
 const app = express()
-const dayTasks = JSON.parse(fs.readFileSync(`${__dirname}/src/data/day-tasks.json`))
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/api/v1/day-tasks', (req, res) => {
+const getTasks = (req, res) => {
   res.send({
     status: 'success',
     data: {
-      dayTasks,
+      todos,
     },
   })
-})
+}
+const getTask = (req, res) => {
+  const todo = todos.find(x => x.id === req.params.id)
+  todo ? success({ statusCode: 200, data: todo, res }) : failure({ statusCode: 404, res })
+}
+const updateTask = (req, res) => {
+  const todo = todos.find(x => x.id === req.params.id)
+  const updatedTodo = todo ? { ...todo, ...req.body } : undefined
+  todo ? success({ statusCode: 200, data: updatedTodo, res }) : failure({ statusCode: 404, res })
+}
+const deleteTask = (req, res) => {
+  const todo = todos.find(x => x.id === req.params.id)
+  todo ? success({ statusCode: 204, res }) : failure({ statusCode: 404, res })
+}
+const addTask = (req, res) => {
+  const newTodo = {
+    id: uuid(),
+    ...req.body,
+  }
+  const updatedTodos = [...todos, newTodo]
 
-app.post('/api/v1/day-tasks', (req, res) => {
-  console.log(req.body)
-  res.send('tsdfsdf')
-})
-// const app = http.createapp((req, res) => {
-//   console.log(req.url)
-//   if (req.url.includes('text')) {
-//     const text = fs.readFile(`${__dirname}/text.json`, 'utf-8', (err, data) => {
-//       const parsed = JSON.parse(data)
-//       res.end(`${parsed.name}, ${parsed.surname}`)
-//     })
-//   } else {
-//     res.end('hello')
-//   }
-// })
+  fs.writeFile(`${__dirname}/src/data/todos.json`, JSON.stringify(updatedTodos), () => {
+    success({ statusCode: 201, data: newTodo, res })
+  })
+}
+
+app.get('/api/v1/todos', getTasks)
+app.get('/api/v1/todos/:id', getTask)
+app.patch('/api/v1/todos/:id', updateTask)
+app.delete('/api/v1/todos/:id', deleteTask)
+app.post('/api/v1/todos', addTask)
 
 app.listen(8000, () => {
   console.log('started on port 8000')
