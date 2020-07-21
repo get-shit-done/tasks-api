a46b2c8ab3865ae7717a536515fad380f9992e52
vincentbollaert <vincentbollaert@gmail.com>
2020-07-21 11:41:58 +0700

    extracted server

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 import express from 'express'
-import fs from 'fs'
 import bodyParser from 'body-parser'
-import path from 'path'
 import morgan from 'morgan'
 
 import { router as tasksRouter } from './src/routes/tasks.js'
 import { router as groupsRouter } from './src/routes/groups.js'
 
-const app = express()
+export const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 app.use('/api/v1/todos', tasksRouter)
 app.use('/api/v1/groups', groupsRouter)
-
-app.listen(8000, () => {
-  console.log('started on port 8000')
-})
