a425ddffdb3c419b5639c32bf812bd9414d71193
vincentbollaert <vincentbollaert@gmail.com>
2020-07-21 11:53:05 +0700

    renamed tasks to todos, added param basic middleware

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 
-import { router as tasksRouter } from './src/routes/tasks.js'
+import { router as todosRouter } from './src/routes/todos.js'
 import { router as groupsRouter } from './src/routes/groups.js'
 
 export const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-app.use('/api/v1/todos', tasksRouter)
+app.use('/api/v1/todos', todosRouter)
 app.use('/api/v1/groups', groupsRouter)
--- src/routes/tasks.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { getTasks, addTask, getTask, updateTask, deleteTask } from '../controllers/tasksController.js'
-
-export const router = express.Router()
-
-router.route('/').get(getTasks).post(addTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
