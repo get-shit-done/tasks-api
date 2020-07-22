560580495985b79af2b08243c84c80ca8a02d3f0
vincentbollaert <vincentbollaert@gmail.com>
2020-07-22 15:40:35 +0700

    changed shape of tasks for scaling

--- src/models/tasksModel.ts
+++ src/models/tasksModel.ts
@@ -1,57 +1,23 @@
 import mongoose from 'mongoose'
 
-// {
-//   "allTasksByDay": {
-//     "1": {
-//       "tasks": [
-//         {
-//           "id": "boig3mGDHYsluEWDtw3x4",
-//           "time": [0, 7],
-//           "name": "sleep",
-//           "group": "essentials"
-//         },
-//         {
-//           "id": "TOCz2b9XPkoI5zqA_K_12",
-//           "time": [7, 7.75],
-//           "name": "lie in bed",
-//           "group": "laze"
-//         },
-//         {
-//           "id": "TkM7FZIzbzPTyjt61z8A_",
-//           "time": [7.75, 8.5],
-//           "name": "morning routine",
-//           "group": "essentials"
-//         },
-//         {
-//           "id": "jJXh7HH6FB-i7nyksOxfK",
-//           "time": [8.5, 8.75],
-//           "name": "get to work",
-//           "group": "essentials"
-//         },
-//         {
-//           "id": "4rWCDcrEYiLOdO4glK_Yf",
-//           "time": [8.75, 11],
-//           "name": "windows admin",
-//           "group": "essentials"
-//         },
-//         {
-//           "id": "6ITo7DIGj8al30zSbOtKI",
-//           "time": [11, 15],
-//           "name": "docker upskill",
-//           "group": "improvement"
-//         },
-//         {
-//           "id": "67vulcplOYLXf4katFKuJ",
-//           "time": [15, 15.5],
-//           "name": "get lunch",
-//           "group": "essentials"
-//         },
-//         {
-//           "id": "tH_0hRXp-oW3pRVbk_5PT",
-//           "time": [15.5, 19.5],
-//           "name": "frontend upskill",
-//           "group": "improvement"
-//         }
-//       ],
-//       "dateString": "Wed Jul 01 2020 00:00:00 GMT+0700 (Indochina Time)"
-//     },
+interface Task {
+  _id: String
+  name: String
+  group: String
+  instances: Object
+}
+const taskSchema = new mongoose.Schema<Task>({
+  name: {
+    type: String,
+    required: [true, 'must have name'],
+    unique: [true, 'must be unique'],
+  },
+  group: {
+    type: String,
+    required: [true, 'must be grouped'],
+    unique: [true, 'must be unique'],
+  },
+  instances: Object,
+})
+
+export const TasksModel = mongoose.model('Task', taskSchema)
