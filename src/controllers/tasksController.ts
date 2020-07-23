0e35945eb5de2e8699e0ca60cb964150d1fee709
vincentbollaert <vincentbollaert@gmail.com>
2020-07-23 09:11:42 +0700

    added task mapping

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,11 +1,50 @@
 import { Request, Response } from 'express'
+import { uuid } from 'uuidv4'
 import { success, failure } from '../utils'
-import { TasksModel } from '../models/tasksModel'
+import { ITask, TasksModel } from '../models/tasksModel'
+
+const tasksResponseMapping = (tasks: ITask[]) => {
+  const mappedData: any = {}
+
+  tasks.forEach(task => {
+    const { id, name, group, instances } = task
+
+    Object.keys(instances).forEach(dayString => {
+      const isMultipleInstances = Array.isArray(instances[dayString][0])
+      const defaultDayShape = { id, tasks: [] }
+      mappedData[dayString] = mappedData[dayString] || defaultDayShape
+      const defaultTaskShape = {
+        id: uuid(),
+        name,
+        group,
+        time: [],
+      }
+
+      if (!isMultipleInstances) {
+        mappedData[dayString].tasks.push({
+          ...defaultTaskShape,
+          time: instances[dayString],
+        })
+      } else {
+        // TODO: fix this any
+        instances[dayString].forEach((inst: any) => {
+          mappedData[dayString].tasks.push({
+            ...defaultTaskShape,
+            time: inst,
+          })
+        })
+      }
+    })
+  })
+
+  return mappedData
+}
 
 export const getTasks = async (req: Request, res: Response) => {
   try {
     const tasks = await TasksModel.find(req.query)
-    success({ statusCode: 200, data: tasks, res })
+    const mappedTasks = tasksResponseMapping(tasks)
+    success({ statusCode: 200, data: mappedTasks, res })
   } catch (error) {
     failure({ statusCode: 500, errorMessage: 'could not find tasks', res })
   }
